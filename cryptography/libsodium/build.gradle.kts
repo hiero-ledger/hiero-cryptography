679f8f5e65d04e09630e797f334482de3ba817c5
Anthony Petrov <anthony@swirldslabs.com>
2026-06-22 13:54:35 -0700

    address comments
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- cryptography/libsodium/build.gradle.kts
+++ cryptography/libsodium/build.gradle.kts
@@ -2,6 +2,7 @@
 import org.gradle.api.internal.file.FileOperations
 import org.gradle.internal.extensions.stdlib.capitalized
 import org.gradle.kotlin.dsl.register
+import org.hiero.gradle.services.TaskLockService
 import org.hiero.gradle.tasks.GitClone
 
 plugins { id("org.hiero.gradle.module.library") }
@@ -15,38 +16,45 @@ tasks.test {
 }
 
 /// Where we check out the libsodium repo from GitHub into the local build/ directory:
-val libDir = layout.buildDirectory.dir("libsodium")
+val libRepositoryDir = layout.buildDirectory.dir("third-party/libsodium")
+val libOutputDir = layout.buildDirectory.dir("third-party/libsodium/output")
 
 tasks.register<GitClone>("cloneLibsodium") {
-    localCloneDirectory = libDir
+    localCloneDirectory = libRepositoryDir
     url = "https://github.com/jedisct1/libsodium.git"
     // branch = "master"
     tag = "1.0.22-RELEASE"
 }
 
 // We cannot build from a single repo for multiple targets at once. So we limit parallelizm:
-abstract class TaskMutex : BuildService<BuildServiceParameters.None>
-
-val mutexService =
-    gradle.sharedServices.registerIfAbsent("taskMutex", TaskMutex::class.java) {
-        maxParallelUsages.set(1)
-    }
+gradle.sharedServices.registerIfAbsent("lock", TaskLockService::class) { maxParallelUsages = 1 }
 
 /// Builds a native library via ./configure && make and copies .so/.dylib/.dll to resources.
+@CacheableTask
 abstract class BuildLibsodiumTask : DefaultTask() {
+    @get:ServiceReference("lock") abstract val lock: Property<TaskLockService>
+
     @get:Inject protected abstract val execOps: ExecOperations
     @get:Inject protected abstract val files: FileOperations
 
     /// Where the native library repo is checked out via GitClone. Must contain ./configure.
-    /// Likely build/<name>/.
-    @get:InputDirectory abstract val libraryDir: DirectoryProperty
+    /// Likely build/third-party/<name>/repository.
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.NONE)
+    abstract val libraryDir: DirectoryProperty
 
     /// ./configure --host ... string, or a blank string to omit the --host arg.
     @get:Input abstract val configureHost: Property<String>
 
     /// Where the binary library to be written.
-    // Likely build/resources/main/com/hedera/nativelib/<name>/<os>/<arch>/.
+    /// Likely build/third-party/<name>/output/<os>-<arch>.
     @get:OutputDirectory abstract val outputDir: DirectoryProperty
+
+    /// Path under the outputDir
+    // Likely com/hedera/nativelib/<name>/<os>/<arch>/
+    // The os/arch tuple must appear twice in both outputDir and outputPath,
+    // because that's how Gradle wants it...
+    @get:Input abstract val outputPath: Property<String>
 
     @TaskAction
     fun action() {
@@ -86,7 +94,7 @@ abstract class BuildLibsodiumTask : DefaultTask() {
                 .flatMap { libExt -> listOf("libsodium?26.${libExt}", "libsodium.${libExt}.26") }
                 .toList()
         val buildDir = libraryDir.get().dir("src/libsodium/.libs")
-        val targetDir = outputDir.get()
+        val targetDir = outputDir.get().dir(outputPath.get())
         println("Copy $filename from $buildDir/ to $targetDir/")
         files.mkdir(targetDir)
         files.sync {
@@ -102,7 +110,7 @@ abstract class BuildLibsodiumTask : DefaultTask() {
         }
         println("Finished copying files.")
         // The output dir w/o the os/arch/ path to print everything we have so far:
-        val resourcesDir = outputDir.get().file("../..").asFile.absolutePath
+        val resourcesDir = outputDir.get().file("..").asFile.absolutePath
         println("Destination listing so far: $resourcesDir")
         execOps.exec { commandLine("ls", "-lR", resourcesDir) }
         println("-----")
@@ -182,33 +190,14 @@ val targets =
 
 targets.forEach { target ->
     val name = "buildLibsodium" + target.os.capitalized() + target.arch.capitalized()
-    println("Registering $name per $target")
     val task =
         tasks.register<BuildLibsodiumTask>(name) {
-            usesService(mutexService)
-            dependsOn("cloneLibsodium")
-            libraryDir = libDir
+            libraryDir = tasks.named<GitClone>("cloneLibsodium").flatMap { it.localCloneDirectory }
             configureHost = target.configureHost
-            outputDir =
-                layout.buildDirectory.dir(
-                    "resources/main/com/hedera/nativelib/libsodium/${target.os}/${target.arch}"
-                )
+            outputDir = libOutputDir.get().dir("${target.os}-${target.arch}")
+            outputPath = "com/hedera/nativelib/libsodium/${target.os}/${target.arch}"
         }
 
-    // Include all built native libraries into the .jar:
-    tasks.jar { from(task) }
-
-    // The compile tasks must also depend on the native builds. Technically, only the local host
-    // target library is required, e.g. to run the code and/or unit tests locally.
-    // But Gradle requires all of them because they all make it into the resources/. So:
-    tasks.compileJava { dependsOn(name) }
-    tasks.compileTestJava { dependsOn(name) }
-}
-
-tasks.named<Jar>("jar") {
-    // Each native library goes into its own os/arch/ subdirectory, and our `ls -lR` proves it.
-    // However, Gradle still complains:
-    //    Entry libsodium.dylib is a duplicate but no duplicate handling strategy has been set.
-    // So we do this:
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    // Include all built native libraries into the .jar and mark them as resources for tests to use:
+    sourceSets["main"].resources.srcDir(task)
 }

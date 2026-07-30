a5df19c5a90cf2b595adb02929e15891f8c618ea
Anthony Petrov <anthony@swirldslabs.com>
2026-07-30 16:50:44 -0700

    toLowerCase
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- common/hedera-common-nativesupport/src/main/java/com/hedera/common/nativesupport/NativeBinary.java
+++ common/hedera-common-nativesupport/src/main/java/com/hedera/common/nativesupport/NativeBinary.java
@@ -131,7 +131,7 @@ public class NativeBinary {
         }
         return SOFTWARE_FOLDER_NAME
                 + RESOURCE_PATH_DELIMITER
-                + libraryName
+                + libraryName.toLowerCase(Locale.US)
                 + RESOURCE_PATH_DELIMITER
                 + os.name().toLowerCase(Locale.US)
                 + RESOURCE_PATH_DELIMITER
@@ -153,8 +153,7 @@ public class NativeBinary {
         if (index == -1 || index == name.length() - 1) {
             return "";
         } else {
-            // Note: assume everything is lower-case as it should be, except for possibly the binary name:
-            return name.substring(0, index).replace('/', '.').toLowerCase(Locale.ROOT);
+            return name.substring(0, index).replace('/', '.');
         }
     }
 
